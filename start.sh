791db5c5feb465e8fd17a4644f199e9843f89b0d
antimodes201 <bob.krause201@gmail.com>
2019-12-12 15:26:02 -0500

    Resolved Wine Conflict

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 
 /steamcmd/steamcmd.sh +login anonymous +force_install_dir /ftn +app_update 764940 +quit
 cd /ftn/Moonlight/Binaries/Win64
-wine MoonlightServer.exe Pittsburgh_Overworld?SessionName="${INSTANCE_NAME}"?listen?Port=${GAME_PORT}?QueryPort=${QUERY_PORT}
+xvfb-run wine MoonlightServer.exe Pittsburgh_Overworld?SessionName="${INSTANCE_NAME}"?listen?Port=${GAME_PORT}?QueryPort=${QUERY_PORT}
